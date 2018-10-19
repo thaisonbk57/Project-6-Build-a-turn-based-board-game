e8d403a170f735efd1478282a6bf5c5af6657e26
Thaison Dang <thaisondang.bk57@gmail.com>
2018-10-19 15:53:13 +0200

    s

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,71 @@
-const [gulp, uglify, autoprefixer, babel, imagemin, connect] = [require("gulp"),
-    require("gulp-uglify"),
-    require("gulp-autoprefixer"),
-    require("gulp-babel"),
-    require("imagemin"),
-    require("gulp-connect")
+const [gulp, uglify, autoprefixer, babel, imagemin, connect] = [
+  require('gulp'),
+  require('gulp-uglify'),
+  require('gulp-autoprefixer'),
+  require('gulp-babel'),
+  require('imagemin'),
+  require('gulp-connect')
 ];
 
 // COPY HTML
 gulp.task('copyHtml', () => {
-    gulp.src("src/index.html")
-        .pipe(gulp.dest("./dist/"))
+  gulp.src('src/index.html').pipe(gulp.dest('./public/'));
 });
-
 
 // MINIFY Photos
 gulp.task('imageMin', () => {
-    gulp.src('scr/img/')
-        .pipe(imagemin())
-        .pipe(gulp.dest("dist/img/"))
+  gulp
+    .src('scr/img/')
+    .pipe(imagemin())
+    .pipe(gulp.dest('public/img/'));
 });
 
 // MINIFY SCRIPTS
 gulp.task('scripts', () => {
-    gulp.src('src/index.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist/js"))
+  gulp
+    .src('src/index.js')
+    .pipe(
+      babel({
+        presets: ['@babel/env']
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js'));
 });
-
 
 // PREFIXER for CSS
 gulp.task('autoprefixer', () =>
-    gulp.src('src/css/style.css')
-    .pipe(autoprefixer({
+  gulp
+    .src('src/css/style.css')
+    .pipe(
+      autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
-    }))
-    .pipe(gulp.dest('dist/css'))
+      })
+    )
+    .pipe(gulp.dest('public/css'))
 );
 
 // WATCH
 gulp.task('watch', () => {
-    gulp.watch('src/**/*.js', ['scripts']);
-    gulp.watch('src/css/style.css', ['autoprefixer']);
-    gulp.watch('src/index.html', ['copyHtml']);
-})
-
-
-// CONNECT
-gulp.task("connect", () => {
-    connect.server({
-        root: "./dist",
-        livereload: true,
-        port: 8888
-    });
+  gulp.watch('src/**/*.js', ['scripts']);
+  gulp.watch('src/css/style.css', ['autoprefixer']);
+  gulp.watch('src/index.html', ['copyHtml']);
 });
 
-gulp.task('default', ['copyHtml', 'autoprefixer', 'scripts', "connect", 'watch']);
+// CONNECT
+gulp.task('connect', () => {
+  connect.server({
+    root: './public',
+    livereload: true,
+    port: 8888
+  });
+});
+
+gulp.task('default', [
+  'copyHtml',
+  'autoprefixer',
+  'scripts',
+  'connect',
+  'watch'
+]);
